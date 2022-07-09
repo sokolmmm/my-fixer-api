5d700f8030d5445ab174027f9acf1cdfdd57049e
sokolmmm <sokol.misha.m@gmail.com>
2022-07-09 17:10:01 +0300

    Add logic to delete the user by id

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -7,5 +7,6 @@ userRouter.get('/users', UserController.usersList);
 userRouter.post('/users', UserController.createUser);
 userRouter.get('/user:userId', UserController.retrieveUserById);
 userRouter.patch('/user:userId', UserController.patchUserById);
+userRouter.delete('/user:userId', UserController.deleteUserById);
 
 export default userRouter;
