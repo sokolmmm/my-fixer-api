c9f95cdb095bb55391e7890f244b0b7259540907
sokolmmm <sokol.misha.m@gmail.com>
2022-07-11 20:50:12 +0300

    Delete dist folder

--- dist/user/controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserController {
-    static async usersList(ctx) {
-        ctx.body = 'user';
-    }
-}
-exports.default = UserController;
-//# sourceMappingURL=controller.js.map
