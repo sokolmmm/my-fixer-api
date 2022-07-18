cac7bdfe974357c5398e07d8af0995ab3431cf7e
sokolmmm <sokol.misha.m@gmail.com>
2022-07-18 10:57:08 +0300

    improve database config

--- src/database/databaseConfig.ts
+++ src/database/databaseConfig.ts
@@ -1,12 +1,15 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
 
 const appSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  database: 'postgres',
-  username: 'sokol',
-  password: '12345',
+  database: process.env.DATABASE,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   synchronize: false,
   logging: false,
   ssl: false,
